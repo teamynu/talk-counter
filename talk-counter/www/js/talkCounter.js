1f5130edb572b428bd38853483248db8b66c79af
ynk23 <ynk23@users.noreply.github.com>
2016-10-31 20:06:15 +0900

    angularJSの形式でjsファイルを記述できるようにindex.htmlを修正(サンプルコードをtalkcounter機能に記載)

---
+++ talk-counter/www/js/talkCounter.js
@@ -0,0 +1,15 @@
+﻿// talkCounter.jsで有効なスコープを即時関数で定義
+(function () {
+    //データを定義
+    var datalist = [
+        { name: "aaa" },
+        { name: "bbb" },
+        { name: "ccc" }
+    ];
+    //talkCounter.htmlの<ons-page ng-controller="TalkCounterController">のスコープの動作を記載
+    app.controller('TalkCounterController', function ($scope) {
+        $scope.name = datalist[0].name;
+    });
+}());
+
+
