e14f1b60a81ed684cf7958a2df2e86435300e25a
ynk23 <ynk23@users.noreply.github.com>
2016-11-07 02:26:30 +0900

    人登録画面の一部を作成

---
+++ talk-counter/www/js/registerHuman.js
@@ -0,0 +1,12 @@
+﻿// registerHuman.jsで有効なスコープを即時関数で定義
+(function () {
+    //$scopeではなくController asの形式を採用
+    app.controller('RegisterHumanController', function () {
+        // 入力された名前を保持
+        this.registname = '';
+        // 登録を押されたときの関数
+        this.submit = function () {
+            console.log('registered!')
+        };
+    });
+}());
