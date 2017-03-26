fc8a67e50e1061d94d18a5d46dccb367069911fc
ynk23 <ynk23@users.noreply.github.com>
2017-03-27 06:37:04 +0900

    人登録と口ぐせ登録を実装完了

--- talk-counter/www/js/registerHuman.js
+++ talk-counter/www/js/registerHuman.js
@@ -4,30 +4,25 @@
     app.controller('RegisterHumanController', function () {
         // 入力された情報を保持
         this.registname = '';
-        this.person = getAllLocalStorageKey();
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
         // 登録を押されたときの関数
         this.submit = function () {
-            if (duplicated(this.person, this.registname)) {
+            if (duplicated(this.datahash, this.registname)) {
                 ons.notification.alert({
                     message: 'すでに登録されています'
                 });
             } else {
-                this.person.push(this.registname);
-                localStorage.setItem(this.registname,[]);
-            }
-        }
-        // ローカルストレージのすべてのkeyを取得
-        function getAllLocalStorageKey() {
-            var ret = [];
-            for (var i = 0; i < localStorage.length; i++) {
-                ret.push(localStorage.key(i));
-            }
-            return ret;
-        }
-        // 配列と追加要素の重複チェック
-        function duplicated(array, item) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i] == item) {
+                this.datahash[this.registname] = {
+                    wordlist: {
+                    }
+                };
+                localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
+            };
+        };
+        // hashと追加要素の重複チェック
+        function duplicated(hash, item) {
+            for (var key in hash) {
+                if (key == item) {
                     return true;
                 }
             }
--- talk-counter/www/js/registerWord.js
+++ talk-counter/www/js/registerWord.js
@@ -1,43 +1,49 @@
 ﻿// registerWord.jsで有効なスコープを即時関数で定義
 (function () {
-    //$scopeではなくController asの形式を採用
-    app.controller('RegisterWordController', function () {
+
+    // ダイアログで人を選択するコントローラ
+    app.controller('SelectPersonDialogContralloer', function ($scope) {
+        // 人名がselectされた時の動作
+        this.select = function (person) {
+            // 親scopeに選んだ人を送信
+            $scope.$emit('selectedPerson', person);
+        };
+    });
+
+    // 登録画面で口ぐせ登録するコントローラ
+    app.controller('RegisterWordController', function ($scope) {
         // 入力された情報を保持
         this.word = '';
-        this.person = getAllLocalStorageKey();
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
         this.selectedPerson = '';
-        this.allwords = [];
-        // 人名がselectされた時の動作
-        this.reload = function (val) {
-            this.allwords = JSON.parse(localStorage.getItem(this.selectedPerson)||"[]");
-        }
+        this.wordlist = {};
+        var $this = this;
+        // 人が選択された時の動作
+        $scope.$on('selectedPerson', function (event, arg) {
+            var person = arg;
+            console.log("person: " + person);
+            $this.selectedPerson = person;
+            $this.wordlist = $this.datahash[person].wordlist;
+        });
         // 登録を押されたときの動作
         this.submit = function () {
-            if (duplicated(this.allwords, this.word)) {
+            if (duplicated(this.wordlist, this.word)) {
                 ons.notification.alert({
                     message: 'すでに登録されています'
                 });
             } else {
-                this.allwords.push(this.word);
-                localStorage.setItem(this.selectedPerson, angular.toJson(this.allwords));
-            }
-        }
-        // ローカルストレージのすべてのkeyを取得
-        function getAllLocalStorageKey() {
-            var ret = [];
-            for (var i=0; i < localStorage.length; i++) {
-                ret.push(localStorage.key(i));
-            }
-            return ret;
-        }
-        // 配列と追加要素の重複チェック
-        function duplicated(array, item) {
-            for (var i = 0; i<array.length; i++) {
-                if (array[i] == item) {
+                this.datahash[this.selectedPerson].wordlist[this.word] = { count: 0 };
+                localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
+            };
+        };
+        // hashと追加要素の重複チェック
+        function duplicated(hash, item) {
+            for (var key in hash) {
+                if (key == item) {
                     return true;
-                }
-            }
+                };
+            };
             return false;
-        }
+        };
     });
 }());
