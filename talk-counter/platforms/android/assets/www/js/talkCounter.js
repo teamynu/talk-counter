cf77464a16e88fab483a217eeb32569e58411d40
ynk23 <ynk23@users.noreply.github.com>
2017-03-28 05:56:48 +0900

    すべての画面実装完了(リファクタリング必要)

--- talk-counter/platforms/android/assets/www/js/talkCounter.js
+++ talk-counter/platforms/android/assets/www/js/talkCounter.js
@@ -1,15 +1,27 @@
 ﻿// talkCounter.jsで有効なスコープを即時関数で定義
 (function () {
-    //データを定義
-    var datalist = [
-        { name: "aaa" },
-        { name: "bbb" },
-        { name: "ccc" }
-    ];
+    //SelectPersonDialogContralloerはregisterWord.jsに記載したため省略
+
     //talkCounter.htmlの<ons-page ng-controller="TalkCounterController">のスコープの動作を記載
     //$scopeではなくController asの形式を採用
-    app.controller('TalkCounterController', function () {
-        this.name = datalist[0].name;
+    app.controller('TalkCounterController', function ($scope) {
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
+        this.selectedPerson = '';
+        this.wordlist = {};
+        var $this = this;
+        // 人が選択された時の動作
+        $scope.$on('selectedPerson', function (event, arg) {
+            var person = arg;
+            console.log("person: " + person);
+            $this.selectedPerson = person;
+            $this.wordlist = $this.datahash[person].wordlist;
+        });
+        // 口ぐせがタップされた時の動作
+        this.addcount = function (word) {
+            var incount = this.datahash[this.selectedPerson].wordlist[word].count;
+            this.datahash[this.selectedPerson].wordlist[word].count = incount + 1;
+            localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
+        }
     });
 }());
 
