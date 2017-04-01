cfeae595cdacc3fcb5d08bfb721f0104114457a2
ynk23 <ynk23@users.noreply.github.com>
2017-04-02 05:48:56 +0900

    トップページからメニューを削除。infinityBubbleのhtmlからメニューを削除。talkCounterのhtmlファイル、jsファイルを統合。

--- talk-counter/platforms/android/assets/www/js/registerword.js
+++ talk-counter/platforms/android/assets/www/js/registerword.js
@@ -1,49 +1,5 @@
 ﻿// registerWord.jsで有効なスコープを即時関数で定義
 (function () {
 
-    // ダイアログで人を選択するコントローラ
-    app.controller('SelectPersonDialogContralloer', function ($scope) {
-        // 人名がselectされた時の動作
-        this.select = function (person) {
-            // 親scopeに選んだ人を送信
-            $scope.$emit('selectedPerson', person);
-        };
-    });
-
-    // 登録画面で口ぐせ登録するコントローラ
-    app.controller('RegisterWordController', function ($scope) {
-        // 入力された情報を保持
-        this.word = '';
-        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
-        this.selectedPerson = '';
-        this.wordlist = {};
-        var $this = this;
-        // 人が選択された時の動作
-        $scope.$on('selectedPerson', function (event, arg) {
-            var person = arg;
-            console.log("person: " + person);
-            $this.selectedPerson = person;
-            $this.wordlist = $this.datahash[person].wordlist;
-        });
-        // 登録を押されたときの動作
-        this.submit = function () {
-            if (duplicated(this.wordlist, this.word)) {
-                ons.notification.alert({
-                    message: 'すでに登録されています'
-                });
-            } else {
-                this.datahash[this.selectedPerson].wordlist[this.word] = { count: 0 };
-                localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
-            };
-        };
-        // hashと追加要素の重複チェック
-        function duplicated(hash, item) {
-            for (var key in hash) {
-                if (key == item) {
-                    return true;
-                };
-            };
-            return false;
-        };
-    });
+    
 }());
--- talk-counter/platforms/android/assets/www/js/talkCounter.js
+++ talk-counter/platforms/android/assets/www/js/talkCounter.js
@@ -1,6 +1,44 @@
 ﻿// talkCounter.jsで有効なスコープを即時関数で定義
 (function () {
-    //SelectPersonDialogContralloerはregisterWord.jsに記載したため省略
+    // データシェアサービス
+    app.factory('dialogService', function () {
+        var dialogs = {};
+        return {
+            show: function (dlg) {
+                if (!dialogs[dlg]) {
+                    ons.createDialog(dlg).then(function (dialog) {
+                        dialogs[dlg] = dialog;
+                        dialog.show();
+                    });
+                } else {
+                    dialogs[dlg].show();
+                };
+            },
+            hide: function (dlg) {
+                dialogs[dlg].hide();
+            }
+        }
+    });
+
+    //　ダイアログコントローラー
+    app.controller('DialogController', function (dialogService) {
+        this.show = function (dlg) {
+            dialogService.show(dlg);
+        }
+        this.hide = function (dlg) {
+            dialogService.hide(dlg);
+        }
+    });
+
+    // ダイアログで人を選択するコントローラ
+    app.controller('SelectPersonDialogController', function ($rootScope, dialogService) {
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
+        // 人名がselectされた時の動作
+        this.select = function (person) {
+            // すべてのscopeに選んだ人を送信
+            $rootScope.$broadcast('selectedPerson', person);
+        };
+    });
 
     //talkCounter.htmlの<ons-page ng-controller="TalkCounterController">のスコープの動作を記載
     //$scopeではなくController asの形式を採用
@@ -22,6 +60,91 @@
             this.datahash[this.selectedPerson].wordlist[word].count = incount + 1;
             localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
         }
+    });
+
+    // registerHuman
+    //$scopeではなくController asの形式を採用
+    app.controller('RegisterPersonController', function () {
+        // 入力された情報を保持
+        this.registname = '';
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
+        // 登録を押されたときの関数
+        this.submit = function () {
+            if (duplicated(this.datahash, this.registname)) {
+                ons.notification.alert({
+                    message: 'すでに登録されています'
+                });
+            } else {
+                this.datahash[this.registname] = {
+                    wordlist: {
+                    }
+                };
+                localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
+            };
+        };
+        // hashと追加要素の重複チェック
+        function duplicated(hash, item) {
+            for (var key in hash) {
+                if (key == item) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    });
+
+    // registerWord
+    // 登録画面で口ぐせ登録するコントローラ
+    app.controller('RegisterWordController', function ($scope) {
+        // 入力された情報を保持
+        this.word = '';
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
+        this.selectedPerson = '';
+        this.wordlist = {};
+        var $this = this;
+        // 人が選択された時の動作
+        $scope.$on('selectedPerson', function (event, arg) {
+            var person = arg;
+            console.log("person: " + person);
+            $this.selectedPerson = person;
+            $this.wordlist = $this.datahash[person].wordlist;
+        });
+        // 登録を押されたときの動作
+        this.submit = function () {
+            if (duplicated(this.wordlist, this.word)) {
+                ons.notification.alert({
+                    message: 'すでに登録されています'
+                });
+            } else {
+                this.datahash[this.selectedPerson].wordlist[this.word] = { count: 0 };
+                localStorage.setItem('wordconterdata', angular.toJson(this.datahash));
+            };
+        };
+        // hashと追加要素の重複チェック
+        function duplicated(hash, item) {
+            for (var key in hash) {
+                if (key == item) {
+                    return true;
+                };
+            };
+            return false;
+        };
+    });
+
+    // showRecord
+    //$scopeではなくController asの形式を採用
+    app.controller('ShowRecordController', function ($scope) {
+        this.datahash = JSON.parse(localStorage.getItem('wordconterdata') || "{}");
+        this.selectedPerson = '';
+        this.wordlist = {};
+        var $this = this;
+        // 人が選択された時の動作
+        $scope.$on('selectedPerson', function (event, arg) {
+            var person = arg;
+            console.log("person: " + person);
+            $this.selectedPerson = person;
+            $this.wordlist = $this.datahash[person].wordlist;
+        });
     });
 }());
 
